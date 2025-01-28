8c841e139f8b33cf9fc536596eb10c32c408e06d
vectronic <hello.vectronic@gmail.com>
2025-01-28 19:15:53 +0000

    fix: fix test

--- tests/cli_test.ts
+++ tests/cli_test.ts
@@ -1,10 +1,10 @@
 import { assertRejects } from "@std/assert";
 import { cli } from "../src/cli.ts";
 
-Deno.test("CLI Test", () => {
-  assertRejects(
+Deno.test("CLI Test", async () => {
+  await assertRejects(
     () => cli(import.meta.url),
-    Error,
     "Test case attempted to exit with exit code: 2",
   );
+  Deno.exitCode = 0;
 });
