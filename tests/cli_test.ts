269e8a7c633a1bb00cc4ba7c620dd38dec88484d
vectronic <hello.vectronic@gmail.com>
2025-01-28 19:07:57 +0000

    fix: fix test

--- tests/cli_test.ts
+++ tests/cli_test.ts
@@ -1,14 +1,6 @@
-import { assertEquals } from "@std/assert";
+import { assertRejects } from "@std/assert";
 import { cli } from "../src/cli.ts";
 
-Deno.test("CLI Test", async () => {
-  try {
-    await cli(import.meta.url);
-  } catch (err) {
-    // expected error: exit code 2 is NO_COMMAND
-    assertEquals(
-      (err as Error).message,
-      "Test case attempted to exit with exit code: 2",
-    );
-  }
+Deno.test("CLI Test", () => {
+  assertRejects(() => cli(import.meta.url), Error, "Test case attempted to exit with exit code: 2");
 });
