54fa419aba68f17c3d727af458f970e158d7197f
vectronic <hello.vectronic@gmail.com>
2025-03-07 22:41:42 +0000

    fix: fix tests

--- src/cli.ts
+++ src/cli.ts
@@ -6,21 +6,21 @@ import {
 } from "@flowscripter/dynamic-cli-framework";
 import command1 from "./commands/command1.ts";
 import command2 from "./commands/command2.ts";
+import packageJson from "../package.json";
 
 /**
  * Run the CLI.
  */
-export async function cli(callingModuleUri: string): Promise<void> {
+export async function cli(): Promise<void> {
   // use launchMultiCommandCLI to show 2 sample commands
   await launchMultiCommandCLI(
     [command1, command2],
     "Simple example CLI using dynamic-cli-framework.",
     `example-cli`, // alternatively, name can be derived from executable name if this is not defined
-    undefined, // version will be derived from callingModuleUrl
+    packageJson.version,
     false, // simple example, so disable env vars support for configuration defaults
     false, // simple example, so disable configuration support
     false, // simple example, so disable key-value service
-    callingModuleUri,
     [
       new BannerServiceProvider(50), // renders an ascii banner on CLI launch
       new AsciiBannerGeneratorServiceProvider(45), // exposes ascii banner generation service
--- tests/cli_test.ts
+++ tests/cli_test.ts
@@ -1,12 +1,16 @@
 import process from "node:process";
-import { describe, expect, test } from "bun:test";
+import { describe, expect, spyOn, test } from "bun:test";
 import { cli } from "../src/cli.ts";
 
 describe("ExampleCli Tests", () => {
   test("CLI Test", async () => {
-    await expect(cli(import.meta.url)).rejects.toThrow(
-      "Test case attempted to exit with exit code: 2",
-    );
-    process.exitCode = 0;
+    const mockExit = spyOn(process, "exit").mockImplementation(() => {
+      throw new Error("Mock exit");
+    });
+
+    await expect(cli()).rejects.toThrow("Mock exit");
+
+    expect(mockExit).toHaveBeenCalledWith(2);
+    mockExit.mockRestore();
   });
 });
