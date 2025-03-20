a2b23faf5c424c3abaa123e6d0b89c80c29a74a1
vectronic <hello.vectronic@gmail.com>
2025-03-20 18:06:19 +0000

    test: remove unneeded async (#14)

--- tests/cli_test.ts
+++ tests/cli_test.ts
@@ -3,12 +3,12 @@ import { describe, expect, spyOn, test } from "bun:test";
 import { cli } from "../src/cli.ts";
 
 describe("ExampleCli Tests", () => {
-  test("CLI Test", async () => {
+  test("CLI Test", () => {
     const mockExit = spyOn(process, "exit").mockImplementation(() => {
       throw new Error("Mock exit");
     });
 
-    await expect(cli()).rejects.toThrow("Mock exit");
+    expect(cli()).rejects.toThrow("Mock exit");
 
     expect(mockExit).toHaveBeenCalledWith(2);
     mockExit.mockRestore();
