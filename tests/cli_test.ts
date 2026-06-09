86ab375ef0ebd8df7f4b2b4722f43b0b4cea2072
vectronic <hello.vectronic@gmail.com>
2026-06-09 12:44:46 +0100

    test: fix test

--- tests/cli_test.ts
+++ tests/cli_test.ts
@@ -3,12 +3,12 @@ import { describe, expect, spyOn, test } from "bun:test";
 import { cli } from "../src/cli.ts";
 
 describe("ExampleCli Tests", () => {
-  test("CLI Test", () => {
+  test("CLI Test", async () => {
     const mockExit = spyOn(process, "exit").mockImplementation(() => {
       throw new Error("Mock exit");
     });
 
-    expect(cli()).rejects.toThrow("Mock exit");
+    await expect(cli()).rejects.toThrow("Mock exit");
 
     mockExit.mockRestore();
   });
